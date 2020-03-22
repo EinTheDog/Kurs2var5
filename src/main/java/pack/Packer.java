92201e269e1b5693f721e80659302ff1d8fe0671
Maxim Alpatski <alpatskij.mi@edu.spbstu.ru>
2020-03-22 08:11:03 +0300

    -z should work

--- src/main/java/pack/Packer.java
+++ src/main/java/pack/Packer.java
@@ -21,27 +21,73 @@ public class Packer {
         try (InputStreamReader reader = new InputStreamReader(in)) {
             try (OutputStreamWriter writer = new OutputStreamWriter(out)) {
                 if (task == Task.packTask) {
-                    int sym = reader.read();
-                    while (sym != -1) {
-                        writer.write(sym);
-                        sym = reader.read();
+                    int unique = 0;
+                    StringBuilder sbUnique = new StringBuilder();
+                    int same = 1;
+                    int cur = reader.read();
+                    int next = reader.read();
+                    while (cur != -1) {
+                        if (cur != next) {
+                            if (same > 1) {
+                                writer.write(String.valueOf(same));
+                                writer.write(cur);
+                                same = 1;
+                            } else {
+                                unique++;
+                                sbUnique.append((char) cur);
+                            }
+                        } else {
+                            if (unique > 0) {
+                                writer.write('-' + String.valueOf(unique));
+                                writer.write(sbUnique.toString());
+                                unique = 0;
+                                sbUnique = new StringBuilder();
+                            }
+                            same++;
+                        }
+                        cur = next;
+                        next = reader.read();
                     }
+                    if (unique > 0) {
+                        writer.write('-' + String.valueOf(unique));
+                        writer.write(sbUnique.toString());
+                    }
+                    if (same > 1) {
+                        writer.write(String.valueOf(same));
+                        writer.write(cur);
+                    }
+
                 } else {
-                    int sym = reader.read();
-                    while (sym != -1) {
-                        writer.write(Character.toUpperCase((char) sym));
-                        sym = reader.read();
+                    int same = 0;
+                    int unique = 0;
+                    int cur = reader.read();
+                    int next = reader.read();
+                    if (cur == '-') {
+                        cur = next;
+                        next = reader.read();
+                        unique = cur;
+                    } else {
+                        same = cur;
+                    }
+                    while (cur != -1) {
+                        while (Character.isDigit(next)) {
+                            if (same > 0) same = same * 10 + next;
+                            else  unique = unique * 10 + next;
+                            cur = next;
+                            next = reader.read();
+                        }
+                        for (int i = 0; i < same; i++) writer.write(next);
+                        for (int i = 0; i < unique; i++) {
+                            writer.write(next);
+                            cur = next;
+                            next = reader.read();
+                        }
                     }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private int count () {
-        int num = 1;
-        while ()
     }
 
     public void pack(String in, String out) throws IOException {
