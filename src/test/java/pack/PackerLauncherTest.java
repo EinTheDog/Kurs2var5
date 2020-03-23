4f4ddc1e1da71422ea4b73049022f29603a9e051
Maxim Alpatski <alpatskij.mi@edu.spbstu.ru>
2020-03-23 20:01:09 +0300

    now make tests finally

--- src/test/java/pack/PackerLauncherTest.java
+++ src/test/java/pack/PackerLauncherTest.java
@@ -3,6 +3,7 @@ package pack;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import com.google.common.io.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PackerLauncherTest {
@@ -11,5 +12,6 @@ class PackerLauncherTest {
     void main() throws IOException {
         String [] args={"-z","-out", "file2.txt", "..\\..\\src\\test\\resources\\file1.txt"};
         File file2 = new File("./output/file2.txt");
+        Files.equal(file2, )
     }
 }
