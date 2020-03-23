fa0b3a216337d2c7d1d7bbf0e0eb58ee75cc4711
Alpatski Maksim <alpatskij.mi@edu.spbstu.ru>
2020-03-23 19:42:11 +0300

    attempt to import guava

--- src/test/java/pack/PackerLauncherTest.java
+++ src/test/java/pack/PackerLauncherTest.java
@@ -10,9 +10,6 @@ class PackerLauncherTest {
     @Test
     void main() throws IOException {
         String [] args={"-z","-out", "file2.txt", "..\\..\\src\\test\\resources\\file1.txt"};
-        PackerLauncher.main(args);
-        BufferedReader reader = new BufferedReader(new FileReader("../../../output/file2.txt"));
-        String s = reader.readLine();
-        assertEquals("2a-4dzdz3b-1f2c", s);
+        File file2 = new File("./output/file2.txt");
     }
 }
